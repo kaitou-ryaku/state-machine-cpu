9d1deedecd61d5e526114c215e515d164dcd4feb
kaitou-ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-12-22 02:21:33 +0900

    メモリーユニットに書き込み機能を追加

--- memory.sv
+++ memory.sv
@@ -1,15 +1,42 @@
 `define MEMSIZE 16
 `define REGSIZE 8
 
-module memory_unit(
-  input logic [`REGSIZE-1:0] address
-  , output logic [`REGSIZE-1:0] value
-);
-  logic [`REGSIZE-1:0] memory [0:`MEMSIZE-1];
-  assign value = memory[address];
+typedef logic [`REGSIZE-1:0] DEFAULT_TYPE;
+typedef enum logic [3:0] {MEMORY_STAY, MEMORY_READ, MEMORY_WRITE} MEMORY_FLAG;
 
-  initial begin
-    $readmemb("test.mem", memory);
+module memory_unit(
+  input    logic        CLOCK
+  , input  logic        RESET
+  , input  MEMORY_FLAG  rw_flag
+  , input  DEFAULT_TYPE address
+  , input  DEFAULT_TYPE write_value
+  , output DEFAULT_TYPE read_value
+);
+  DEFAULT_TYPE initial_memory [0:`MEMSIZE-1];
+  initial $readmemb("test.mem", initial_memory);
+
+  DEFAULT_TYPE memory [0:`MEMSIZE-1];
+  assign read_value = memory[address];
+
+  DEFAULT_TYPE next_memory [0:`MEMSIZE-1];
+  always_comb begin
+    if (rw_flag == MEMORY_WRITE) begin
+      next_memory = {
+        memory[0:address-`REGSIZE'd1],
+        write_value,
+        memory[address+`REGSIZE'd1:`MEMSIZE-1]
+      };
+    end else begin
+      next_memory = memory;
+    end
+  end
+
+  always_ff @(posedge CLOCK) begin
+    if (RESET) begin
+      memory <= initial_memory;
+    end else begin
+      memory <= next_memory;
+    end
   end
 
 endmodule
