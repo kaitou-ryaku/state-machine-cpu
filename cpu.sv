1552e24b9fb56fe1eb20cd0aa6222772e52725ac
kaitou-ryaku <34281612+kaitou-ryaku@users.noreply.github.com>
2018-12-25 03:06:22 +0900

    FETCH_OPERATIONの内部ステートマシン化

--- cpu.sv
+++ cpu.sv
@@ -10,11 +10,14 @@ module cpu(/*{{{*/
   , output DEFAULT_TYPE     OUT
 );
   DEFAULT_TYPE next_write_bus;
+  STAGE_TYPE stage, next_stage;
 
-  STATE_TYPE state, next_state;
+  STAGE_FETCH_OPERATION_TYPE stage_fetch_operation;
+  //STAGE_FETCH_IMMEDIATE_TYPE stage_fetch_immediate;
+
   DEFAULT_TYPE ip, next_ip;
 
-  DEFAULT_TYPE ope, next_ope;
+  DEFAULT_TYPE ope;
   OPECODE_TYPE decode_ope;
   OPERAND_TYPE decode_src, decode_dst;
   decoder decoder0(.*);
@@ -42,13 +45,15 @@ module cpu(/*{{{*/
   update_memory_addr_bus update_memory_addr_bus0(.*);
   update_memory_flag update_memory_flag0(.*);
 
-  update_state update_state0(.*);
+  update_stage update_stage0(.*);
   update_execution_result update_execution_result0(.*);
-  update_ip  update_ip0(.*);
   update_memory_src update_memory_src(.*);
   update_memory_dst update_memory_dst(.*);
 
-  update_ope update_ope0(.*);
+  DEFAULT_TYPE next_ip_operation;
+  update_stage_operation update_stage_operation0(.*, .next_ip(next_ip_operation));
+  update_ip  update_ip0(.*);
+
   update_imm update_imm0(.*);
 
   clock_posedge clock_posedge0(.*);
@@ -68,7 +73,8 @@ module decoder(/*{{{*/
       `REGSIZE'b0000????: decode_ope = MOV;
       `REGSIZE'b0001????: decode_ope = ADD;
       `REGSIZE'b1100????: decode_ope = JMP;
-      `REGSIZE'b1111????: decode_ope = HLT;
+      `REGSIZE'b11111110: decode_ope = NOP;
+      `REGSIZE'b11111111: decode_ope = HLT;
       default:            decode_ope = HLT;
     endcase
 
@@ -132,7 +138,7 @@ module decoder_dst(/*{{{*/
 endmodule/*}}}*/
 
 module alu(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input OPECODE_TYPE decode_ope
   , input  DEFAULT_TYPE src
   , input  DEFAULT_TYPE original_dst
@@ -140,7 +146,7 @@ module alu(/*{{{*/
 );
 
   always_comb begin
-    unique case (state)
+    unique case (stage)
       EXECUTE: begin
         unique case (decode_ope)
           ADD:     next_dst = original_dst+src;
@@ -171,57 +177,61 @@ module jmp_addr_bus(/*{{{*/
 
 endmodule/*}}}*/
 
-module update_state(/*{{{*/
-  input STATE_TYPE state
+module update_stage(/*{{{*/
+  input STAGE_TYPE stage
+  , input STAGE_FETCH_OPERATION_TYPE stage_fetch_operation
   , input OPERAND_TYPE decode_src
   , input OPERAND_TYPE decode_dst
-  , output STATE_TYPE next_state
+  , output STAGE_TYPE next_stage
 );
 
   always_comb begin
-    unique case (state)
-      RESET_STATE: next_state = FETCH_OPERATION;
+    unique case (stage)
+      RESET_STAGE: next_stage = FETCH_OPERATION;
 
-      FETCH_OPERATION: next_state = DECODE;
+      FETCH_OPERATION: begin
+        if (stage_fetch_operation == END_FETCH_OPERATION) next_stage = DECODE;
+        else next_stage = FETCH_OPERATION;
+      end
 
       DECODE: begin
-        if      (decode_src == IMM)           next_state = FETCH_IMMEDIATE;
-        else if (decode_src == ADDRESS_IMM)   next_state = FETCH_SRC_IMM;
-        else if (decode_src == ADDRESS_REG_A) next_state = FETCH_SRC;
-        else if (decode_dst == ADDRESS_IMM)   next_state = FETCH_DST_IMM;
-        else if (decode_dst == ADDRESS_REG_A) next_state = FETCH_DST;
-        else                                  next_state = EXECUTE;
+        if      (decode_src == IMM)           next_stage = FETCH_IMMEDIATE;
+        else if (decode_src == ADDRESS_IMM)   next_stage = FETCH_SRC_IMM;
+        else if (decode_src == ADDRESS_REG_A) next_stage = FETCH_SRC;
+        else if (decode_dst == ADDRESS_IMM)   next_stage = FETCH_DST_IMM;
+        else if (decode_dst == ADDRESS_REG_A) next_stage = FETCH_DST;
+        else                                  next_stage = EXECUTE;
       end
 
-      FETCH_SRC_IMM: next_state = FETCH_SRC;
+      FETCH_SRC_IMM: next_stage = FETCH_SRC;
 
       FETCH_SRC, FETCH_IMMEDIATE: begin
-        if      (decode_dst == ADDRESS_IMM)   next_state = FETCH_DST_IMM;
-        else if (decode_dst == ADDRESS_REG_A) next_state = FETCH_DST;
-        else                                  next_state = EXECUTE;
+        if      (decode_dst == ADDRESS_IMM)   next_stage = FETCH_DST_IMM;
+        else if (decode_dst == ADDRESS_REG_A) next_stage = FETCH_DST;
+        else                                  next_stage = EXECUTE;
       end
 
-      FETCH_DST_IMM: next_state = FETCH_DST;
+      FETCH_DST_IMM: next_stage = FETCH_DST;
 
-      FETCH_DST: next_state = EXECUTE;
+      FETCH_DST: next_stage = EXECUTE;
 
-      EXECUTE: next_state = WRITE_REGISTER;
+      EXECUTE: next_stage = WRITE_REGISTER;
 
       WRITE_REGISTER: begin
-        if (decode_dst == ADDRESS_IMM) next_state = WRITE_MEMORY;
-        else                           next_state = FETCH_OPERATION;
+        if (decode_dst == ADDRESS_IMM) next_stage = WRITE_MEMORY;
+        else                           next_stage = FETCH_OPERATION;
       end
 
-      WRITE_MEMORY: next_state = FETCH_OPERATION;
+      WRITE_MEMORY: next_stage = FETCH_OPERATION;
 
-      default: next_state = FETCH_OPERATION;
+      default: next_stage = FETCH_OPERATION;
     endcase
   end
 
 endmodule/*}}}*/
 
 module update_execution_result(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input OPERAND_TYPE decode_dst
   , input  DEFAULT_TYPE dst
   , input  DEFAULT_TYPE register_a
@@ -230,7 +240,7 @@ module update_execution_result(/*{{{*/
 );
 
   always_comb begin
-    if (state == WRITE_REGISTER) begin
+    if (stage == WRITE_REGISTER) begin
       unique case (decode_dst)
         REG_A: begin
           next_register_a = dst;
@@ -262,9 +272,10 @@ module update_execution_result(/*{{{*/
 endmodule/*}}}*/
 
 module update_ip(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input DEFAULT_TYPE ip
   , input OPECODE_TYPE decode_ope
+  , input DEFAULT_TYPE next_ip_operation
   , input DEFAULT_TYPE jmp
   , output DEFAULT_TYPE next_ip
 );
@@ -273,8 +284,8 @@ module update_ip(/*{{{*/
       next_ip = ip;
 
     end else begin
-      unique case (state)
-        FETCH_OPERATION: next_ip = ip + `REGSIZE'b1;
+      unique case (stage)
+        FETCH_OPERATION: next_ip = next_ip_operation;
         FETCH_IMMEDIATE: next_ip = ip + `REGSIZE'b1;
         FETCH_SRC_IMM:   next_ip = ip + `REGSIZE'b1;
         FETCH_DST_IMM:   next_ip = ip + `REGSIZE'b1;
@@ -285,22 +296,8 @@ module update_ip(/*{{{*/
   end
 endmodule/*}}}*/
 
-module update_ope(/*{{{*/
-  input STATE_TYPE state
-  , input  DEFAULT_TYPE ope
-  , input  DEFAULT_TYPE read_bus
-  , output DEFAULT_TYPE next_ope
-);
-  always_comb begin
-    unique case (state)
-      FETCH_OPERATION: next_ope = read_bus;
-      default:         next_ope = ope;
-    endcase
-  end
-endmodule/*}}}*/
-
 module update_imm(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input  DEFAULT_TYPE read_bus
   , input  DEFAULT_TYPE imm
   , input  DEFAULT_TYPE imm_src
@@ -310,21 +307,21 @@ module update_imm(/*{{{*/
   , output DEFAULT_TYPE next_imm_dst
 );
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_IMMEDIATE: next_imm = read_bus;
       default:         next_imm = imm;
     endcase
   end
 
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_SRC_IMM: next_imm_src = read_bus;
       default:       next_imm_src = imm_src;
     endcase
   end
 
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_DST_IMM: next_imm_dst = read_bus;
       default:       next_imm_dst = imm_dst;
     endcase
@@ -332,13 +329,13 @@ module update_imm(/*{{{*/
 endmodule/*}}}*/
 
 module update_memory_src(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input  DEFAULT_TYPE read_bus
   , input  DEFAULT_TYPE memory_src
   , output DEFAULT_TYPE next_memory_src
 );
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_SRC: next_memory_src = read_bus;
       default:   next_memory_src = memory_src;
     endcase
@@ -346,13 +343,13 @@ module update_memory_src(/*{{{*/
 endmodule/*}}}*/
 
 module update_memory_dst(/*{{{*/
-  input STATE_TYPE state
+  input STAGE_TYPE stage
   , input  DEFAULT_TYPE read_bus
   , input  DEFAULT_TYPE memory_dst
   , output DEFAULT_TYPE next_memory_dst
 );
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_DST: next_memory_dst = read_bus;
       default:   next_memory_dst = memory_dst;
     endcase
@@ -360,7 +357,7 @@ module update_memory_dst(/*{{{*/
 endmodule/*}}}*/
 
 module update_memory_addr_bus(/*{{{*/
-  input    STATE_TYPE       state
+  input    STAGE_TYPE       stage
   , input  DEFAULT_TYPE     ip
   , input  OPERAND_TYPE     decode_src
   , input  OPERAND_TYPE     decode_dst
@@ -370,7 +367,7 @@ module update_memory_addr_bus(/*{{{*/
   , output DEFAULT_TYPE     addr_bus
 );
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_OPERATION: addr_bus = ip;
       FETCH_IMMEDIATE: addr_bus = ip;
       FETCH_SRC_IMM:   addr_bus = ip;
@@ -397,11 +394,11 @@ module update_memory_addr_bus(/*{{{*/
 endmodule/*}}}*/
 
 module update_memory_flag(/*{{{*/
-  input    STATE_TYPE       state
+  input    STAGE_TYPE       stage
   , output MEMORY_FLAG_TYPE ctrl_bus
 );
   always_comb begin
-    unique case (state)
+    unique case (stage)
       FETCH_OPERATION: ctrl_bus = MEMORY_READ;
       FETCH_IMMEDIATE: ctrl_bus = MEMORY_READ;
       FETCH_SRC_IMM:   ctrl_bus = MEMORY_READ;
@@ -418,38 +415,35 @@ module clock_posedge(/*{{{*/
   input logic CLOCK
   , input logic RESET
 
-  , input  STATE_TYPE   next_state
-  , input  DEFAULT_TYPE next_ip
-  , input  DEFAULT_TYPE next_ope
-  , input  DEFAULT_TYPE next_imm
-  , input  DEFAULT_TYPE next_imm_src
-  , input  DEFAULT_TYPE next_imm_dst
-  , input  DEFAULT_TYPE next_memory_src
-  , input  DEFAULT_TYPE next_memory_dst
-  , input  DEFAULT_TYPE next_register_a
-  , input  DEFAULT_TYPE next_dst
-  , input  DEFAULT_TYPE next_original_dst
-  , input  DEFAULT_TYPE next_write_bus
+  , input  STAGE_TYPE       next_stage
+  , input  DEFAULT_TYPE     next_ip
+  , input  DEFAULT_TYPE     next_imm
+  , input  DEFAULT_TYPE     next_imm_src
+  , input  DEFAULT_TYPE     next_imm_dst
+  , input  DEFAULT_TYPE     next_memory_src
+  , input  DEFAULT_TYPE     next_memory_dst
+  , input  DEFAULT_TYPE     next_register_a
+  , input  DEFAULT_TYPE     next_dst
+  , input  DEFAULT_TYPE     next_original_dst
+  , input  DEFAULT_TYPE     next_write_bus
 
-  , output STATE_TYPE   state
-  , output DEFAULT_TYPE ip
-  , output DEFAULT_TYPE ope
-  , output DEFAULT_TYPE imm
-  , output DEFAULT_TYPE imm_src
-  , output DEFAULT_TYPE imm_dst
-  , output DEFAULT_TYPE memory_src
-  , output DEFAULT_TYPE memory_dst
-  , output DEFAULT_TYPE register_a
-  , output DEFAULT_TYPE dst
-  , output DEFAULT_TYPE original_dst
-  , output DEFAULT_TYPE write_bus
+  , output STAGE_TYPE       stage
+  , output DEFAULT_TYPE     ip
+  , output DEFAULT_TYPE     imm
+  , output DEFAULT_TYPE     imm_src
+  , output DEFAULT_TYPE     imm_dst
+  , output DEFAULT_TYPE     memory_src
+  , output DEFAULT_TYPE     memory_dst
+  , output DEFAULT_TYPE     register_a
+  , output DEFAULT_TYPE     dst
+  , output DEFAULT_TYPE     original_dst
+  , output DEFAULT_TYPE     write_bus
 
 );
   always_ff @(posedge CLOCK) begin
     unique if (RESET) begin
-      state        <= RESET_STATE;
+      stage        <= RESET_STAGE;
       ip           <= `REGSIZE'b0;
-      ope          <= `REGSIZE'b0;
       imm          <= `REGSIZE'b0;
       imm_src      <= `REGSIZE'b0;
       imm_dst      <= `REGSIZE'b0;
@@ -461,9 +455,8 @@ module clock_posedge(/*{{{*/
       write_bus    <= `REGSIZE'b0;
 
     end else begin
-      state        <= next_state;
+      stage        <= next_stage;
       ip           <= next_ip;
-      ope          <= next_ope;
       imm          <= next_imm;
       imm_src      <= next_imm_src;
       imm_dst      <= next_imm_dst;
@@ -475,4 +468,70 @@ module clock_posedge(/*{{{*/
       write_bus    <= next_write_bus;
     end
   end
+endmodule/*}}}*/
+
+module update_stage_operation(/*{{{*/
+  input logic CLOCK
+  , input logic RESET
+  , input STAGE_TYPE stage
+  , input  DEFAULT_TYPE read_bus
+  , input  DEFAULT_TYPE ip
+  , output STAGE_FETCH_OPERATION_TYPE stage_fetch_operation
+  , output DEFAULT_TYPE next_ip
+  , output DEFAULT_TYPE ope
+);
+  STAGE_FETCH_OPERATION_TYPE next_stage_fetch_operation;
+
+  always_comb begin
+    unique case (stage)
+      FETCH_OPERATION: begin
+        unique case (stage_fetch_operation)
+          BGN_FETCH_OPERATION: next_stage_fetch_operation = END_FETCH_OPERATION;
+          END_FETCH_OPERATION: next_stage_fetch_operation = IDL_FETCH_OPERATION;
+          IDL_FETCH_OPERATION: next_stage_fetch_operation = BGN_FETCH_OPERATION;
+        endcase
+      end
+      default: next_stage_fetch_operation = IDL_FETCH_OPERATION;
+    endcase
+  end
+
+  DEFAULT_TYPE next_ope;
+  always_comb begin
+    case (stage)
+      FETCH_OPERATION: begin
+        case (stage_fetch_operation)
+          BGN_FETCH_OPERATION: begin
+            next_ope = read_bus;
+            next_ip  = ip;
+          end
+
+          END_FETCH_OPERATION: begin
+            next_ope = ope;
+            next_ip  = ip + `REGSIZE'd1;
+          end
+
+          default: begin
+            next_ope = ope;
+            next_ip  = ip;
+          end
+        endcase
+      end
+
+      default: begin
+        next_ope = ope;
+        next_ip  = ip;
+      end
+    endcase
+  end
+
+  always_ff @(posedge CLOCK) begin
+    unique if (RESET) begin
+      stage_fetch_operation <= IDL_FETCH_OPERATION;
+      ope <= `BYTE_NOP_OPECODE;
+    end else begin
+      stage_fetch_operation <= next_stage_fetch_operation;
+      ope <= next_ope;
+    end
+  end
+
 endmodule/*}}}*/
